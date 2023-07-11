bc16cc4e492ad9f534671618e0f71357a8135e88
Desislav Arashev <darashev@cst-bg.net>
2023-07-11 16:13:36 +0300

    Fix backup/restore scripts

--- scripts/data-backup/backup-data.sh
+++ scripts/data-backup/backup-data.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 NAMESPACE="default"  # Change to the namespace where your PVCs are located
 LOCAL_DIR="./backup" # Change to the local directory where you want to store the backup
 
@@ -10,13 +12,17 @@ PVCS=$(kubectl get pvc -n "${NAMESPACE}" -o jsonpath='{.items[*].metadata.name}'
 PVCS=$(echo "${PVCS}" | tr " " "\n" | grep "^claim-")
 
 for PVC in ${PVCS}; do
-  # Create a temporary pod for each PVC
-  POD_YAML=$(cat <<EOF
+  (
+    # LOGFILE="${LOCAL_DIR}/${PVC}_backup.log"
+    # exec &> >(tee -a "${LOGFILE}")
+
+    # Create a temporary pod for each PVC
+    POD_YAML=$(cat <<EOF
 apiVersion: v1
 kind: Pod
 metadata:
   name: temp-pod-${PVC}
-  namespace: "${NAMESPACE}"
+  namespace: ${NAMESPACE}
 spec:
   containers:
   - name: temp-pod
@@ -33,19 +39,22 @@ spec:
 EOF
 )
 
-  echo "${POD_YAML}" | kubectl apply -f -
+    echo "${POD_YAML}" | kubectl apply -f -
 
-  echo "Waiting for pod temp-pod-${PVC} to be ready..."
+    echo "Waiting for pod temp-pod-${PVC} to be ready..."
 
-  while [[ $(kubectl get pods temp-pod-$PVC -n "${NAMESPACE}" -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do 
-    echo "waiting..." && sleep 5; 
-  done
+    while [[ $(kubectl get pods temp-pod-$PVC -n "${NAMESPACE}" -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do 
+      echo "waiting..." && sleep 5; 
+    done
 
-  # Compress data
-  kubectl exec -it temp-pod-"${PVC}" -n "${NAMESPACE}" -- apk add tar && kubectl exec -it temp-pod-"${PVC}" -n "${NAMESPACE}" -- tar -czvf /data.tar.gz /data 
-  kubectl cp "${NAMESPACE}"/temp-pod-"${PVC}":/data.tar.gz "${LOCAL_DIR}"/"${PVC}".tar.gz
+    # Compress data
+    kubectl exec -it temp-pod-"${PVC}" -n "${NAMESPACE}" -- apk add tar && kubectl exec -it temp-pod-"${PVC}" -n "${NAMESPACE}" -- tar -czf /data.tar.gz -C /data . && echo "Backup successful" || echo "Backup failed"
+    kubectl cp "${NAMESPACE}"/temp-pod-"${PVC}":/data.tar.gz "${LOCAL_DIR}"/"${PVC}".tar.gz && echo "Copy successful" || echo "Copy failed"
 
-  # Delete the temporary pod
-  kubectl delete pod temp-pod-"${PVC}" -n "${NAMESPACE}"
-
+    # Delete the temporary pod
+    kubectl delete pod temp-pod-"${PVC}" -n "${NAMESPACE}"
+  ) &
 done
+
+# Wait for all background jobs to finish
+wait
--- scripts/data-backup/restore-data.sh
+++ scripts/data-backup/restore-data.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
-NAMESPACE="default"  # Change to the namespace where your PVCs are located
+set -e
+
+NAMESPACE="alternative"  # Change to the namespace where your PVCs are located
 LOCAL_DIR="./backup" # Change to the local directory where you want to store the backup
 
-# Get a list of all PVCs in the namespace
-PVCS=$(kubectl get pvc -n "${NAMESPACE}" -o jsonpath='{.items[*].metadata.name}')
+# get list of pvcs from the persistentvolumeclaim folder
+PVCS=$(ls -l ./persistentvolumeclaim | awk '{print $9}')
+
+# Remve the .yaml suffix from the list of pvcs
+PVCS=$(echo $PVCS | tr " " "\n" | sed 's/.yaml//g')
 
 for PVC in ${PVCS}; do
-  # Create a temporary pod for each PVC
-  POD_YAML=$(cat <<EOF
+  (
+    # LOGFILE="${LOCAL_DIR}/${PVC}_restore.log"
+    # exec &> >(tee -a "${LOGFILE}")
+
+    # Create a temporary pod for each PVC
+    POD_YAML=$(cat <<EOF
 apiVersion: v1
 kind: Pod
 metadata:
-  name: temp-pod-"${PVC}"
-  namespace: "${NAMESPACE}"
+  name: temp-pod-${PVC}
+  namespace: ${NAMESPACE}
 spec:
   containers:
   - name: temp-pod
@@ -26,22 +35,25 @@ spec:
   volumes:
   - name: volume
     persistentVolumeClaim:
-      claimName: "${PVC}"
+      claimName: ${PVC}
 EOF
 )
 
-  echo "${POD_YAML}" | kubectl apply -f -
+    echo "${POD_YAML}" | kubectl apply -f -
 
-  echo "Waiting for pod temp-pod-${PVC} to be ready..."
+    echo "Waiting for pod temp-pod-${PVC} to be ready..."
 
-  while [[ $(kubectl get pods temp-pod-"${PVC}" -n "${NAMESPACE}" -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do 
-    echo "waiting..." && sleep 5; 
-  done
+    while [[ $(kubectl get pods temp-pod-$PVC -n "${NAMESPACE}" -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do 
+      echo "waiting..." && sleep 5; 
+    done
 
-  # Restore data
-  kubectl cp "${LOCAL_DIR}"/"${PVC}".tar.gz "${NAMESPACE}"/temp-pod-"${PVC}":/data.tar.gz
-  kubectl exec -it temp-pod-"${PVC}" -n "${NAMESPACE}" -- apk add tar && kubectl exec -it temp-pod-"${PVC}" -n "${NAMESPACE}" -- tar -xzvf /data.tar.gz -C /data
+    kubectl cp "${LOCAL_DIR}"/"${PVC}".tar.gz "${NAMESPACE}"/temp-pod-"${PVC}":/data.tar.gz && echo "Copy successful" || echo "Copy failed"
+    kubectl exec -it temp-pod-"${PVC}" -n "${NAMESPACE}" -- apk add tar && kubectl exec -it temp-pod-"${PVC}" -n "${NAMESPACE}" -- tar -xzvf /data.tar.gz -C /data && echo "Restore successful" || echo "Restore failed"
 
-  # Delete the temporary pod
-  kubectl delete pod temp-pod-"${PVC}" -n "${NAMESPACE}"
+    # Delete the temporary pod
+    kubectl delete pod temp-pod-"${PVC}" -n "${NAMESPACE}"
+  ) &
 done
+
+# Wait for all background jobs to finish
+wait
